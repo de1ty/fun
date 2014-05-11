c7826f32c3065a977fd809c878748959f04773e9
de1ty <mlcntnt@gmail.com>
2014-05-10 22:15:44 -0400

    first commit from the command line ~~

---
+++ biebergame.js
@@ -0,0 +1,34 @@
+// Check if the user is ready to play!
+age = prompt("What's your age?");
+if(age < 13)
+{
+    //action
+    console.log("You can play, but I take no responsibility.");
+}
+else //otherwise
+{
+    //reaction
+    console.log("Prepare yourself !! It's your turn!");
+    
+}
+
+alert("Are you ready to play?");
+confirm("I'm so fucking ready!");
+
+console.log("You are at a Justin Bieber concert, and you hear this lyric 'Lace my shoes off, start racing.'");
+
+//first spoken lines
+console.log("Suddenly, Bieber stops and says, 'Who wants to race me?'");
+
+//declare variable for user answer
+var userAnswer = prompt("Do you want to race Bieber on stage?");
+
+//if else sequence
+if (userAnswer = "yes") {
+    // do yes
+    console.log("You and Bieber start racing. It's neck and neck! You win by a shoelace!");
+}
+else {
+    //do no
+    console.log("Oh no! Bieber shakes his head and sings 'I set a pace, so I can race without pacing.'");
+}
